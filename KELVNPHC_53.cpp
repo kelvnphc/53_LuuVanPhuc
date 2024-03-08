0df1909a4546eb73196251009cc16e7ab314fc61
kelvnphc <luuvanphucc13072003@gmail.com>
2024-03-08 17:02:09 +0700

    Them 1 dong vao c.pp

--- KELVNPHC_53.cpp
+++ KELVNPHC_53.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
 	cout << "main \n";
+	cout << "====\n";
 	cout << "Chao cac ban IT2101!\n"
 	cout << "Happy International Women's Day 8-3-2024 \n"
 	system("pause");
