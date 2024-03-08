6bfb410707b9c169939be6ac52d5c263bbbe2882
kelvnphc <luuvanphucc13072003@gmail.com>
2024-03-08 16:58:50 +0700

    Them 1 dong

--- KELVNPHC_53.cpp
+++ KELVNPHC_53.cpp
@@ -2,17 +2,9 @@
 using namespace std;
 int main()
 {
-<<<<<<< HEAD
-	cout << "Main conflict \n";
+	cout << "main \n";
 	cout << "Chao cac ban IT2101!\n"
 	cout << "Happy International Women's Day 8-3-2024 \n"
-=======
-	cout << "Conflict ne \n";
-	cout << "Chao cac ban IT2101!\n"
-		cout << "Happy International Women's Day 8-3-2024 \n";
-
-
->>>>>>> VanPhuc-new
 	system("pause");
 	return 0;
 }
